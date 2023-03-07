24112e6667be7e24062a3ad6cbcf46c9467930f0
eiwos <mahmoudi.otman@gmail.com>
2023-03-07 20:32:40 +0100

    Features: Adding the 'search a card' block in home

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import AddForm from '@/components/add-form.jsx';
+import SearchForm from '@/components/search-form.jsx';
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,6 +13,12 @@ export default function Home() {
         <details>
           <summary>Add a card</summary>
           <AddForm></AddForm>
+        </details>
+      </article>
+      <article>
+        <details>
+          <summary>Search a card</summary>
+          <SearchForm></SearchForm>
         </details>
       </article>
     </div>
