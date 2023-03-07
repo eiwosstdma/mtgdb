9cf333d4fbdb482bc78746f54f4ef198a8a8de2d
eiwos <mahmoudi.otman@gmail.com>
2023-03-07 17:34:33 +0100

    Features: Adding a 'add form' to the layout

---
+++ components/add-form.jsx
@@ -0,0 +1,21 @@
+export default function AddForm() {
+  return (
+    <form action="/add" method="post">
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" name="name" required/>
+
+      <label htmlFor="color">Name</label>
+      <input type="text" id="color" name="color" required/>
+
+      <label htmlFor="type">Name</label>
+      <input type="text" id="type" name="type" required/>
+
+      <label htmlFor="file">Image</label>
+      <input type="file" id="file" name="file" required/>
+
+      <br/>
+      <hr/>
+      <button type="submit">Add a card</button>
+    </form>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import AddForm from '@/components/add-form.jsx';
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <div>
+      <article>
+        <details>
+          <summary>Add a card</summary>
+          <AddForm></AddForm>
+        </details>
+      </article>
+    </div>
   )
 }
