1f8e1c4831bddbecd477b4e6aa1cd42396379de9
eiwos <mahmoudi.otman@gmail.com>
2023-03-08 20:28:09 +0100

    Fix: Forms did not have selects for type/color

--- components/add-form.jsx
+++ components/add-form.jsx
@@ -4,11 +4,29 @@ export default function AddForm() {
       <label htmlFor="name">Name</label>
       <input type="text" id="name" name="name" required/>
 
-      <label htmlFor="color">Name</label>
-      <input type="text" id="color" name="color" required/>
+      <label htmlFor="color">Color</label>
+      <select name="color" id="color" required>
+        <option></option>
+        <option value="colorless">Colorless</option>
+        <option value="multicolor">Multicolor</option>
+        <option value="white">White</option>
+        <option value="blue">Blue</option>
+        <option value="black">Black</option>
+        <option value="red">Red</option>
+        <option value="green">Green</option>
+      </select>
 
-      <label htmlFor="type">Name</label>
-      <input type="text" id="type" name="type" required/>
+      <label htmlFor="type">Type</label>
+      <select name="type" id="type" required>
+        <option></option>
+        <option value="creature">Creature</option>
+        <option value="instant">Instant</option>
+        <option value="enchantment">Enchantment</option>
+        <option value="artifact">Artifact</option>
+        <option value="sorcery">Sorcery</option>
+        <option value="planeswalker">Planeswalker</option>
+        <option value="land">Land</option>
+      </select>
 
       <label htmlFor="file">Image</label>
       <input type="file" id="file" name="file" required/>
