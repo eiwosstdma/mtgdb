95d62de6e149849cdfd627132ff429d80d142d4e
eiwos <mahmoudi.otman@gmail.com>
2023-03-09 15:32:12 +0100

    Update: Delete the image associated to the card locally too.

--- pages/delete/[id].js
+++ pages/delete/[id].js
@@ -1,3 +1,6 @@
+import { join } from 'node:path';
+import { rm } from 'node:fs/promises';
+
 import CardModel from '@/utils/card.model.js';
 
 export default function ID({ message }) {
@@ -14,6 +17,9 @@ export async function getServerSideProps(context) {
   try {
     const DB = await CardModel()._instance;
     const Cards = await DB.model('cards');
+    const card = await Cards.findById({ _id: id }).exec();
+    const fileSubPath = card.image;
+    await rm(join(process.cwd(), 'public', fileSubPath));
     await Cards.deleteOne({ _id: id });
 
     return {
